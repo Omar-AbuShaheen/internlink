91e032ad0f8caeb78e956640ddc1e09c6f3c7a17
Omar-AbuShaheen <omar.ssaheen7@gmail.com>
2025-05-04 01:32:19 +0300

    Add AdminPanel style import

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,11 +1,13 @@
+import React from 'react';
+import '../styles/AdminPanel.css';
+
 function AdminPanel() {
-    return (
-      <div>
-        <h1>Admin Panel</h1>
-        <p>Oversee user activity and manage system settings.</p>
-      </div>
-    );
-  }
-  
-  export default AdminPanel;
-  
+  return (
+    <div className="admin-panel">
+      <h1>Admin Panel</h1>
+      <p>Monitor platform activity and manage users.</p>
+    </div>
+  );
+}
+
+export default AdminPanel;
