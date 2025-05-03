32ad643905f75bf0b3e4839057415dcd461caabc
Omar-AbuShaheen <omar.ssaheen7@gmail.com>
2025-05-04 01:01:39 +0300

    Add CompanyDashboard component

---
+++ src/components/CompanyDashboard.js
@@ -0,0 +1,11 @@
+function CompanyDashboard() {
+    return (
+      <div>
+        <h1>Company Dashboard</h1>
+        <p>Post and manage internship listings and view applicants.</p>
+      </div>
+    );
+  }
+  
+  export default CompanyDashboard;
+  
