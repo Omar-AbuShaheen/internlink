a67cbe6c3dff036a86ef51399a93c922c13c0e64
Omar-AbuShaheen <omar.ssaheen7@gmail.com>
2025-05-04 02:43:54 +0300

    Update App.js to include NavigationBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <HomePage />
     </div>
   );
