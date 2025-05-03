2d73890df4db6f28f46f4d19275eba85be9d90e9
Omar-AbuShaheen <omar.ssaheen7@gmail.com>
2025-05-04 01:02:31 +0300

    Add AdminPanel component

---
+++ src/components/AdminPanel.js
@@ -0,0 +1,11 @@
+function AdminPanel() {
+    return (
+      <div>
+        <h1>Admin Panel</h1>
+        <p>Oversee user activity and manage system settings.</p>
+      </div>
+    );
+  }
+  
+  export default AdminPanel;
+  
