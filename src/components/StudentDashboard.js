9dcb1739350d84749dd337d802ccd189dd054c74
Omar-AbuShaheen <omar.ssaheen7@gmail.com>
2025-05-04 01:00:34 +0300

    Add StudentDashboard component

---
+++ src/components/StudentDashboard.js
@@ -0,0 +1,11 @@
+function StudentDashboard() {
+    return (
+      <div>
+        <h1>Student Dashboard</h1>
+        <p>View and manage your profile and internship applications.</p>
+      </div>
+    );
+  }
+  
+  export default StudentDashboard;
+  
