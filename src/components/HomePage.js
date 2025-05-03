2cb6478cb66b9bd1bbb57eb23e41aaa5e84c152f
Omar-AbuShaheen <omar.ssaheen7@gmail.com>
2025-05-04 01:28:45 +0300

    Add HomePage style import

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Welcome to InternLink</h1>
+      <p>Your platform to connect students with internships.</p>
+    </div>
+  );
+}
+
+export default HomePage;
