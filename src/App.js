dbf2ccef145073a0e7dc3c208b1c005b69cef3ce
Omar-AbuShaheen <omar.ssaheen7@gmail.com>
2025-05-08 23:06:35 +0300

    feat: add CompanyDashboard with listings and routing

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import AboutUs from './components/AboutUs';
 import InternshipList from './components/InternshipList';
 import InternshipDetail from './components/InternshipDetail';
 import StudentDashboard from './components/StudentDashboard';
+import CompanyDashboard from './components/CompanyDashboard';
+
 
 
 
@@ -28,6 +30,8 @@ function App() {
           <Route path="/internships/:id" element={<InternshipDetail />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/student/dashboard" element={<StudentDashboard />} />
+          <Route path="/company/dashboard" element={<CompanyDashboard />} />
+
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/services" element={<Services />} />
           <Route path="/about" element={<AboutUs />} />
